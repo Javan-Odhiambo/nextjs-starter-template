fec7d3a5c1867faa7b9e63586240dd5bfb103ad2
Javan Odhiambo <114527546+Javan-Odhiambo@users.noreply.github.com>
2024-05-01 23:11:48 +0000

    refactor(shared): add basic styling to the InputField component

--- src/components/ui/InputField.tsx
+++ src/components/ui/InputField.tsx
@@ -1,21 +1,32 @@
+import { cn } from '@/lib/utils';
 import React from 'react';
 import { FieldError } from 'react-hook-form';
 
 type InputFieldProps = {
-  label?: string;
-  id: string;
-  register: any;
-  error?: FieldError | null;
+    label?: string;
+    id: string;
+    register: any;
+    error?: FieldError | null;
 };
 
 const InputField: React.FC<InputFieldProps> = ({ label, id, register, error }) => {
-  return (
-    <div>
-      {label && <label htmlFor={id}>{label}:</label>}
-      <input id={id} {...register} />
-      {error && <div>{error.message}</div>}
-    </div>
-  );
+
+    const hasErrors = error != null
+
+    const inputClassName = cn(
+        "border-2 rounded-md w-full",
+        { "border-2 border-red-500 focus:border-red-500 focus:ring-red-500": hasErrors },
+    )
+
+    return (
+        <div className="w-full flex gap-3">
+            {label && <label htmlFor={id}>{label}:</label>}
+            <div className='flex flex-col'>
+                <input className={inputClassName} id={id} {...register} />
+                {error && <div className="text-red-500 text-sm" >{error.message}</div>}
+            </div>
+        </div>
+    );
 };
 
 export default InputField;
