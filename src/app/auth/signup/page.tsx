cd0aaeb3ed1f36bea559f2b693d8dbd3e0b2f8a2
Javan Odhiambo <114527546+Javan-Odhiambo@users.noreply.github.com>
2024-05-01 22:08:57 +0000

    feat(tools): add react-hook-form sample

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,54 +1,63 @@
 "use client"
-import React , { useState} from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import { z } from 'zod'; // Import Zod
+import { zodResolver } from '@hookform/resolvers/zod';
+import { error } from 'console';
 
-const user = z.object({
-    name: z.string().min(2).max(50),
-    email: z.string().email(),
-    password: z.string()
-      .min(6)
-      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, 'Password must contain at least one uppercase letter and one digit'),
+
+//Should be imported from the types file.
+type User = {
+    name: string,
+    email: string,
+    password: string,
+}
+
+//Define the schema for the form
+const userSchema = z.object({
+  name: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z.string()
+    .min(6)
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, 'Password must contain at least one uppercase letter and one digit'),
+});
+
+
+const SignUpPage = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm<User>({
+    resolver: zodResolver(userSchema),
   });
 
-const SignUpForm = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-      });
-    
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        try {
-            const validData = user.safeParse(formData);
-            console.log(validData);
-        } catch (err) {
-            if (err instanceof z.ZodError) {
-              console.log(err.issues);
-            }
-          }
-    };
+  //Function that handles submision of validated data
+  const onSubmit = async (data: User) => {
+    console.log(data);
+    // Submit the data to your API or perform any other action
+  };
 
-    return (
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-        <h1>Sign Up</h1>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" />
-            </div>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" />
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" />
-            </div>
-            <button type="submit">Sign Up</button>
-        </form>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" {...register('name')} />
+          {errors.name && <div>{errors.name.message}</div>}
         </div>
-    );
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" {...register('email')} />
+          {errors.email && <div>{errors.email.message}</div>}
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" {...register('password')} />
+          {errors.password && <div>{errors.password.message}</div>}
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 };
 
-export default SignUpForm;
+export default SignUpPage;
